118924012c2921c868737f627b30129e0338fdbc
prajapatilalit <prajapatilalit150992@gmail.com>
2021-10-25 20:02:07 +0530

    update song detail component

--- songs-collection/src/components/App.js
+++ songs-collection/src/components/App.js
@@ -1,10 +1,18 @@
 import React from "react";
+import SongDetail from "./SongDetail";
 import SongsList from "./SongsList";
 
 const App = () => {
   return (
-    <div>
-      <SongsList />
+    <div className="ui container grid ">
+      <div className="ui row">
+        <div className="column eight wide">
+          <SongsList />
+        </div>
+        <div className="column eight wide">
+          <SongDetail />
+        </div>
+      </div>
     </div>
   );
 };
