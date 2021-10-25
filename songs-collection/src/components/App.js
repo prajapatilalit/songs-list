330861d8cbf42f9202304d4bc7b9a9c8f08e8004
prajapatilalit <prajapatilalit150992@gmail.com>
2021-10-25 21:28:09 +0530

    update song detail component

--- songs-collection/src/components/App.js
+++ songs-collection/src/components/App.js
@@ -4,13 +4,21 @@ import SongsList from "./SongsList";
 
 const App = () => {
   return (
-    <div className="ui container grid ">
-      <div className="ui row">
-        <div className="column eight wide">
-          <SongsList />
-        </div>
-        <div className="column eight wide">
-          <SongDetail />
+    <div
+      style={{ padding: "20px", backgroundColor: "#ce5c62", height: "100vh" }}
+    >
+      <h2 className="ui center aligned icon header">
+        <i className="music icon"></i>
+        Song List
+      </h2>
+      <div className="ui container grid segment">
+        <div className="ui row">
+          <div className="column eight wide">
+            <SongsList />
+          </div>
+          <div className="column eight wide">
+            <SongDetail />
+          </div>
         </div>
       </div>
     </div>
--- songs-collection/src/components/SongDetail.js
+++ songs-collection/src/components/SongDetail.js
@@ -1,8 +1,20 @@
 import React from "react";
 import { connect } from "react-redux";
 
-const SongDetail = () => {
-  return <div>SongDetail</div>;
+const SongDetail = ({ song }) => {
+  if (!song) {
+    return <div className="ui segment">Select a Song</div>;
+  }
+  return (
+    <div className="ui segment">
+      <h3>Detail for :-</h3>
+      <p>
+        <strong>Title : </strong> {song.title}
+        <br />
+        <strong>Duration : </strong> {song.duration}
+      </p>
+    </div>
+  );
 };
 
 const mapStateToProps = (state) => {
